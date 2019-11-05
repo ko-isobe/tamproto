f0030b3459eadfc7e40bdc4d5a02a9e4973fd318
Isobe Kohei <isobekohei@gmail.com>
2019-11-05 19:12:41 +0900

    implement tam endpoint

--- routes/apis.js
+++ routes/apis.js
@@ -1,10 +1,55 @@
  var express = require('express');
  var router = express.Router();
+ var jws = require('jws');
 
  router.get('/',function(req,res,next){
     var param = {"値":"This is sample"};
     res.header('Content-Type','application/json; charset=utf-8');
     res.send(param);
+ });
+
+ var teepImplReturn = true;
+
+ router.post('/tam',function(req,res,next){
+   // check POST content
+      console.log(req.headers);
+
+   // pass the TAM's OTrP Implementation (ProcessConnect?)
+
+   //teepImplReturn = true;
+
+   //GetDeviceStateTBSRequest
+   var teepObj = {
+      "GetDeviceStateTBSRequest":{
+      "ver":"1.0",
+      "rid" : "000000",
+      "tid" : "000001",
+      "ocspdat" : "0x0000",
+      }
+   };
+
+   
+   //var teepSignedObj = jws.sign({
+   //   header: {alg:},
+   //})
+
+   // response 
+
+   res.set({
+      'Content-Type':'application/teep+json',
+      'Cache-Control':'no-store',
+      'X-Content-Type-Options':'nosniff',
+      'Content-Security-Policy': "default-src 'none'",
+      'Referrer-Policy':'no-referrer'
+   });
+   if(teepImplReturn){
+      res.send(JSON.stringify(teepObj));
+      teepImplReturn=false;
+   }else{
+      res.set(null);
+      res.status(204).send('no content');
+   }
+
  });
 
  module.exports = router;
