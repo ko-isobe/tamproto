72e998b0c39074c5acfd923340290e71828d2984
Isobe Kohei <isobekohei@gmail.com>
2019-11-06 16:59:21 +0900

    TLS Support

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var http = require('http');
+var https = require('https');
+var fs = require('fs');
 
 var apis = require('./routes/apis');
 var panels = require('./routes/panels');
 
 var app = express();
+
+var opts = {
+    key: fs.readFileSync('./key/TAM_key.pem'),
+    cert: fs.readFileSync('./key/TAM_cert.pem'),
+    ca: fs.readFileSync('./key/RootCA_crt2.pem'),
+    //requestCert:true,
+    //rejectUnauthorized: false
+  };
 
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,6 +28,10 @@ app.use('/panel',panels);
 
 var listener = http.createServer(app).listen(8888, function(){
     console.log('Express HTTP server');
+});
+
+var tls_listener = https.createServer(opts, app).listen(8433, function () {
+    console.log('Express HTTPS server listening on port ' + tls_listener.address().port);
 });
 
 module.exports = app;
