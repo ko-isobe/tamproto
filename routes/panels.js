9954865a16a17d680d7f111f66663d0a46678fb4
Isobe Kohei <isobekohei@gmail.com>
2019-11-11 18:06:52 +0900

    add TA Manage panels

--- routes/panels.js
+++ routes/panels.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var jws = require('jws');
+var fs = require('fs');
 
-router.get('/',function(req,res,next){
-    res.render("./index.ejs");
+router.get('/', function (req, res, next) {
+    var fileList;
+
+    fs.readdir('./TAs', { withFileTypes: true }, function (err, files) {
+        if (err) throw err;
+        fileList = files;
+        console.log(fileList);
+        res.locals.files = fileList;
+        res.render("./index.ejs");
+    });
+
+    //await makeRequest();
+    //console.log(fileList);
+    
 });
 
 module.exports = router;
