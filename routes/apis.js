8e1a843f7f9303a1f8e6e643ce842a6a4355ec19
Isobe Kohei <isobekohei@gmail.com>
2019-11-12 16:53:13 +0900

    porting from AIST impl

--- routes/apis.js
+++ routes/apis.js
@@ -2,6 +2,26 @@ var express = require('express');
 var router = express.Router();
 var jws = require('jws');
 var otrp = require('../otrp.js');
+var jose = require('node-jose');
+var fs = require('fs');
+
+var keystore = jose.JWK.createKeyStore();
+var tee_pubkey = fs.readFileSync("./key/spaik-pub.jwk", function(err, data) {
+   console.log(data);
+});
+
+var tam_privkey = fs.readFileSync("./key/TAM_key.pem", function(err, data) {
+   console.log(data);
+});
+
+var jwk_tam_privkey, jwk_tee_pubkey;
+
+keystore.add(tee_pubkey, "json").then(function(result) {
+		jwk_tee_pubkey = result;
+        });
+keystore.add(tam_privkey, "pem").then(function(result) {
+		jwk_tam_privkey = result;
+        });
 
 router.get('/', function (req, res, next) {
    var param = { "key": "This is sample" };
@@ -51,6 +71,65 @@ router.post('/tam', function (req, res, next) {
       res.status(204).send('no content');
       return;
    }
+});
+
+router.post('/install',function(req,res,next){
+   // check POST content
+   console.log(req.headers);
+   console.log(req.body);
+   var f_pt = fs.readFileSync("./TAs/dummy2.ta",function(err,data){
+      console.log(err);
+   });
+
+   jose.JWE.createEncrypt({ alg: 'RSA1_5', contentAlg: 'A128CBC-HS256', format: "flattened" },jwk_tee_pubkey)
+      .update(f_pt).final().then(function(result) {
+         f = JSON.stringify(result);
+
+         jose.JWS.createSign({ alg: 'RS256', format: 'flattened' }, jwk_tam_privkey)
+            .update(f).final().then(function(result){
+               f = JSON.stringify(result);
+               res.statusCode = 200;
+               res.set({
+                  'Content-Type': 'application/teep+json',
+                  'Cache-Control': 'no-store',
+                  'X-Content-Type-Options': 'nosniff',
+                  'Content-Security-Policy': "default-src 'none'",
+                  'Referrer-Policy': 'no-referrer'
+               });
+               res.setHeader('Content-Length', f.length);
+               res.end(f);
+      });      
+   });
+});
+
+router.post('/delete',function(req,res,next){
+   // check POST content
+   console.log(req.headers);
+   console.log(req.body);
+
+   var cmd = "{\"delete-ta\":\"8d82573a-926d-4754-9353-32dc29997f74.ta\"}";
+   console.log("Request for delete packet\n");
+
+   jose.JWE.createEncrypt({ alg: 'RSA1_5', contentAlg: 'A128CBC-HS256', format: "flattened" },
+         jwk_tee_pubkey).update(cmd).final().then
+            (function(result) {
+      f = JSON.stringify(result);
+
+      jose.JWS.createSign({ alg: 'RS256', format: 'flattened' }, jwk_tam_privkey).update(f).final().then
+               (function(result) {
+         f = JSON.stringify(result);
+         res.statusCode = 200;
+         res.set({
+            'Content-Type': 'application/teep+json',
+            'Cache-Control': 'no-store',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': "default-src 'none'",
+            'Referrer-Policy': 'no-referrer'
+         });
+         res.setHeader('Content-Length', f.length);
+         res.end(f);
+      });
+   });
 });
 
 module.exports = router;
